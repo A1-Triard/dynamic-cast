67cda708f1e4d79fc7d7d221932c7d2be85f729e
Master <internalmike@gmail.com>
2025-06-05 15:20:12 +0300

    T -> Is, update

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 The fifth pillar of OOP: dynamic casting.
 
+(If you are looking for the rest, check out the [basic-oop](https://crates.io/crates/basic-oop) crate.)
+
 ```rust
 use dynamic_cast::{SupportsInterfaces, impl_supports_interfaces, dyn_cast_arc};
 use std::sync::Arc;
@@ -11,7 +13,7 @@ use std::sync::Arc;
 // class Base
 
 #[portrait::make]
-trait TBase: SupportsInterfaces {
+trait IsBase: SupportsInterfaces {
     fn base_method(&self) -> u8;
 }
 
@@ -19,16 +21,16 @@ struct Base {
     data: u8,
 }
 
-impl_supports_interfaces!(Base: TBase);
+impl_supports_interfaces!(Base: IsBase);
 
-impl TBase for Base {
+impl IsBase for Base {
     fn base_method(&self) -> u8 { self.data }
 }
 
 // class Descendant: Base
 
 #[portrait::make]
-trait TDescendant: TBase + SupportsInterfaces {
+trait IsDescendant: IsBase + SupportsInterfaces {
     fn descendant_method(&self) -> u8;
 }
 
@@ -37,24 +39,24 @@ struct Descendant {
     data: u8,
 }
 
-impl_supports_interfaces!(Descendant: TBase, TDescendant);
+impl_supports_interfaces!(Descendant: IsBase, IsDescendant);
 
 #[portrait::fill(portrait::delegate(Base; self.base))]
-impl TBase for Descendant { }
+impl IsBase for Descendant { }
 
-impl TDescendant for Descendant {
+impl IsDescendant for Descendant {
     fn descendant_method(&self) -> u8 { self.data }
 }
 
 // casting
 
 fn main() {
-    let a: Arc<dyn TDescendant> = Arc::new(Descendant { base: Base { data: 1 }, data: 2 });
+    let a: Arc<dyn IsDescendant> = Arc::new(Descendant { base: Base { data: 1 }, data: 2 });
     assert_eq!(a.descendant_method(), 2);
     assert_eq!(a.base_method(), 1);
-    let a_as_base: Arc<dyn TBase> = dyn_cast_arc(a).unwrap();
+    let a_as_base: Arc<dyn IsBase> = dyn_cast_arc(a).unwrap();
     assert_eq!(a_as_base.base_method(), 1);
-    let a_as_descendant: Arc<dyn TDescendant> = dyn_cast_arc(a_as_base).unwrap();
+    let a_as_descendant: Arc<dyn IsDescendant> = dyn_cast_arc(a_as_base).unwrap();
     assert_eq!(a_as_descendant.descendant_method(), 2);
     assert_eq!(a_as_descendant.base_method(), 1);
 }
--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -1,15 +1,14 @@
-#![feature(start)]
-
 #![deny(warnings)]
 #![allow(unused_macros)]
 
 #![no_std]
+#![no_main]
 
 extern crate alloc;
 
 #[cfg(windows)]
 #[link(name="msvcrt")]
-extern { }
+extern "C" { }
 
 mod no_std {
     use composable_allocators::{AsGlobal, System};
@@ -34,7 +33,7 @@ use dynamic_cast::{SupportsInterfaces, impl_supports_interfaces, dyn_cast_box};
 // class Base
 
 #[portrait::make]
-trait TBase: SupportsInterfaces {
+trait IsBase: SupportsInterfaces {
     fn base_method(&self) -> u8;
 }
 
@@ -42,16 +41,16 @@ struct Base {
     data: u8,
 }
 
-impl_supports_interfaces!(Base: TBase);
+impl_supports_interfaces!(Base: IsBase);
 
-impl TBase for Base {
+impl IsBase for Base {
     fn base_method(&self) -> u8 { self.data }
 }
 
 // class Descendant: Base
 
 #[portrait::make]
-trait TDescendant: TBase + SupportsInterfaces {
+trait IsDescendant: IsBase + SupportsInterfaces {
     fn descendant_method(&self) -> u8;
 }
 
@@ -60,25 +59,27 @@ struct Descendant {
     data: u8,
 }
 
-impl_supports_interfaces!(Descendant: TBase, TDescendant);
+impl_supports_interfaces!(Descendant: IsBase, IsDescendant);
 
 #[portrait::fill(portrait::delegate(Base; self.base))]
-impl TBase for Descendant { }
+impl IsBase for Descendant { }
 
-impl TDescendant for Descendant {
+impl IsDescendant for Descendant {
     fn descendant_method(&self) -> u8 { self.data }
 }
 
 // main
 
-#[start]
-fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    let a: Box<dyn TDescendant> = Box::new(Descendant { base: Base { data: 1 }, data: 2 });
+use core::ffi::{c_int, c_char};
+
+#[no_mangle]
+extern "C" fn main(_argc: c_int, _argv: *mut *mut c_char) -> c_int {
+    let a: Box<dyn IsDescendant> = Box::new(Descendant { base: Base { data: 1 }, data: 2 });
     assert_eq!(a.descendant_method(), 2);
     assert_eq!(a.base_method(), 1);
-    let a_as_base: Box<dyn TBase> = dyn_cast_box(a).unwrap();
+    let a_as_base: Box<dyn IsBase> = dyn_cast_box(a).unwrap();
     assert_eq!(a_as_base.base_method(), 1);
-    let a_as_descendant: Box<dyn TDescendant> = dyn_cast_box(a_as_base).unwrap();
+    let a_as_descendant: Box<dyn IsDescendant> = dyn_cast_box(a_as_base).unwrap();
     assert_eq!(a_as_descendant.descendant_method(), 2);
     assert_eq!(a_as_descendant.base_method(), 1);
     0
